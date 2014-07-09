ececc390cb0fde0cf1bf3112c3287044a0341944
Popescu Gheorghe <digiedu2@digital-education.com>
2014-07-09 10:44:13 +0300

    Cleaned the code

--- HeapSort/heapsort_partial.rb
+++ HeapSort/heapsort_partial.rb
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
+
 # Main clas
 class Array
 	def heapsort
 		self.dup.heapsort #creates a copy in order to not alter the object used
   end
-
-# HEAPIFY function
-def heapify(start, end_)
-	root = start
-	min, left, right  = root, root*2, root*2+1
-=======
-class Array
->>>>>>> 8d27a0a3122043f7b025f3886a6d896c4f04f4c2
 
 	# HEAPIFY function
 	def heapify(start, end_)
@@ -29,14 +21,5 @@ class Array
 		if min != root
 			self[min], self[root] = self[root], self[min]
 			heapify(min, end_)
-		end	
+		end
 	end
-
-<<<<<<< HEAD
-	if min != root
-		self[min], self[root] = self[root], self[min]
-		heapify(min, end_)
-	end
-=======
->>>>>>> 8d27a0a3122043f7b025f3886a6d896c4f04f4c2
-end
